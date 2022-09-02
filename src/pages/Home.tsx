1471edb2ac43904a3daa1e245d188c5dab23f2a5
hugosouza <hugosouzaoficial4@gmail.com>
2022-09-02 09:43:02 -0300

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,26 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+
+import '../styles/auth.scss'
+
+
 export function Home() {
   return(
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Toda pergunta tem resposta</strong>
         <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
       <main>
-        <div>
+        <div className='main-content'>
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button className='create-room'>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className='separator'>ou entre em uma sala</div>
           <form>
             <input 
               type="text"
