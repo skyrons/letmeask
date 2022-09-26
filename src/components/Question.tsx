0de00d6024748031a41b5656b6c3637a128eb238
hugosouza <hugosouzaoficial4@gmail.com>
2022-09-26 20:24:48 -0300

    Likes on question function has created

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import '../styles/questions.scss'
 
 type QuestionProps = {
@@ -6,11 +7,13 @@ type QuestionProps = {
     name: string,
     avatar: string,
   }
+  children?: ReactNode;
 }
 
 export function Question({
   content, 
   author,
+  children,
 } : QuestionProps) {
   return(
     <div className="question">
@@ -19,8 +22,8 @@ export function Question({
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{ author.name }</span>
-          <div></div>
         </div>
+        <div>{children}</div>
       </footer>
     </div>
   );
